ed251f44a7f7c73f54f31f34349dcc41cbbec84d
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-08-05 10:06:12 +0200

    Automatic synchronization from INGInious

--- m1stack/StackProps.scala
+++ m1stack/StackProps.scala
@@ -5,34 +5,34 @@ import java.util.EmptyStackException
 
 object StackProps extends Properties("Stack") {
 	property("empty") = Prop.forAll { (el: String) =>
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		s.push(el)
 		s.pop()
 		s.empty()
 	}
 
 	property("not empty 1") = Prop.forAll { (el: String) =>
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		s.push(el)
 		!s.empty()
 	}
 
 	property("not empty 2") = Prop.forAll { (el: String) =>
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		s.push(el)
 		s.peek()
 		!s.empty()
 	}
 
 	property("double push") = Prop.forAll { (el1: String, el2: String) =>
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		s.push(el1)
 		s.push(el2)
 		(s.pop().equals(el2) && s.peek().equals(el1) && !s.empty())
 	}
 
 	property("multiple push") = Prop.forAll { (el: String, n: Int) =>
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		for (i <- 1 to n%100)
 			s.push(el)
 		for (i <- 1 to n%100)
@@ -41,17 +41,17 @@ object StackProps extends Properties("Stack") {
 	}
 
 	property("pop exception") =  {
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		Prop.throws(classOf[EmptyStackException]) {s.pop()}
 	}
 
 	property("peek exception") = {
-		var s = new MyStack()
+		var s = new MyStack<String>()
 		Prop.throws(classOf[EmptyStackException]) {s.peek()}
 	}
 
 	property("LIFO order") = Prop.forAll { ( elements: List[String]) =>
-    	var s = new MyStack()
+    	var s = new MyStack<String>()
     	elements.foreach(el => s.push(el))
     	elements.reverse.forall{_ == s.pop()}
   	}
