aeef4f61aceb397e6d4c612574390f2fd9f341ab
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-08-06 10:27:12 +0200

    Automatic synchronization from INGInious

--- m1interpretertests/Interpreter.java
+++ m1interpretertests/Interpreter.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.EmptyStackException;
+import java.lang.ArithmeticException;
 import java.util.Arrays;
 
 
@@ -16,11 +17,11 @@ public class Interpreter {
 	/**
 	 * Méthode principale
 	 * @param args
-	 *
+	 */
 	public static void main(String[] args) {
 		Interpreter interpreter = new Interpreter(); 
 		System.out.println(interpreter.interpret("1 1 add pstack")); 
-	}*/
+	}
 	
 	public Interpreter() {
 		stack = new MyStack<Element>();
@@ -184,7 +185,7 @@ public class Interpreter {
 		}
 	}
 	
-	public static void div(MyStack<Element> stack, Hashtable<String,Element> variables) {
+	public static void div(MyStack<Element> stack, Hashtable<String,Element> variables) throws ArithmeticException {
 		Object o1 = null;
 		Object o2 = null;
 		try{
@@ -197,8 +198,13 @@ public class Interpreter {
 			if (o2 == null)
 				o2 = 0 ;
 		}
-		double div = Double.valueOf(""+o2) / Double.valueOf(""+o1);
-				stack.push(new Element(""+div));
+		if (Double.valueOf(""+o1) == 0) {
+			throw new ArithmeticException();
+		}
+		else {
+			double div = Double.valueOf(""+o2) / Double.valueOf(""+o1);
+			stack.push(new Element(""+div));
+		}
 		
 	}
 	
@@ -268,13 +274,254 @@ public class Interpreter {
 		}
 	}
 	
-	public static void pop(MyStack<Element> stack, Hashtable<String,Element> variables) {
-		try{
-			stack.pop();
-		}catch(EmptyStackException e)
-		{
-			e.printStackTrace();
-		}
+	public static void pop(MyStack<Element> stack, Hashtable<String,Element> variables) throws EmptyStackException {
+		stack.pop(); 
 	}
 
+}
+
+class MyStack<E> { 
+	
+	private Node<E> top;
+	private int size;
+	
+	public MyStack() {
+		top = null;
+		size = 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void push(E element) {
+		Node<E> newNode = new Node<E>(element, top);
+		top = newNode;
+		size++;
+	}
+	
+	public E top() throws EmptyStackException {
+		if(isEmpty()) throw new EmptyStackException();
+		return top.getElement();
+	}
+
+	public E pop() throws EmptyStackException {
+		if(isEmpty()) throw new EmptyStackException();
+		E tmp = top.getElement();
+		top = top.getNext();
+		size--;
+		return tmp;
+	}
+	
+	public String toString()
+	{
+		String toString = "";
+		Node<E> tmp = top;
+		while (tmp != null)
+		{
+			if (!toString.equals("")) toString += " "; 
+			toString += tmp.toString(); 
+			tmp = tmp.getNext();
+		}
+		return toString;
+	}
+
+class Node<E> {
+
+	private E element;
+	private Node<E> next;
+	
+	public Node(E element, Node<E> next) {
+		this.element = element;
+		this.next = next;
+	}
+	
+	public E getElement() {
+		return this.element;
+	}
+	
+	public Node<E> getNext() {
+		return this.next;
+	}
+	
+	public void setElement(E element) {
+		this.element = element;
+	}
+	
+	public void setNext(Node<E> next) {
+		this.next = next;
+	}
+	
+	public String toString()
+	{
+		return element.toString();
+	}
+}
+
+}
+
+class Element {
+
+	private static String TYPE_OPERATOR = "Operator";
+	private static String TYPE_INT = "Int";
+	private static String TYPE_DOUBLE = "Double";
+	private static String TYPE_BOOLEAN = "Boolean";
+	private static String TYPE_VARIABLE = "Variable";
+	
+	private String type;
+	private String element;
+	private double double_value;
+	private int int_value;
+	
+	public Element(String elem)
+	{
+		element = elem;
+		if(elem.contains("/"))
+		{
+			type = TYPE_VARIABLE;
+		}
+		else if (elem.contains("."))
+		{
+			type = TYPE_DOUBLE;
+			double_value = Double.parseDouble(elem);
+		}
+		else if (elem.contains("1")||elem.contains("2")||elem.contains("3")||elem.contains("4")||elem.contains("5")
+				||elem.contains("6")||elem.contains("7")||elem.contains("8")||elem.contains("9")||elem.contains("0"))
+		{
+			type = TYPE_INT;
+			int_value = Integer.parseInt(elem);
+		}
+		else if (elem.contains("pstack"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("add"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("sub"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("mul"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("div"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("dup"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("exch"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("eq"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("ne"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("def"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("pop"))
+		{
+			type = TYPE_OPERATOR;
+		}
+		else if (elem.contains("true"))
+		{
+			type = TYPE_BOOLEAN;
+		}
+		else if (elem.contains("false"))
+		{
+			type = TYPE_BOOLEAN;
+		}
+		else type = TYPE_VARIABLE;
+	}
+	
+	public Object interpret(MyStack<Element> stack, ArrayList<String> results, Hashtable<String,Element> variables)
+	{
+		if (type == TYPE_OPERATOR)
+		{
+			if (element.contains("pstack"))
+			{
+				Interpreter.pstack(stack, results, variables);
+			}
+			else if (element.contains("add"))
+			{
+				Interpreter.add(stack, variables);
+			}
+			else if (element.contains("sub"))
+			{
+				Interpreter.sub(stack, variables);
+			}
+			else if (element.contains("mul"))
+			{
+				Interpreter.mul(stack, variables);
+			}
+			else if (element.contains("div"))
+			{
+				Interpreter.div(stack, variables);
+			}
+			else if (element.contains("dup"))
+			{
+				Interpreter.dup(stack, variables);
+			}
+			else if (element.contains("exch"))
+			{
+				Interpreter.exch(stack, variables);
+			}
+			else if (element.contains("eq"))
+			{
+				Interpreter.eq(stack, variables);
+			}
+			else if (element.contains("ne"))
+			{
+				Interpreter.ne(stack, variables);
+			}
+			else if (element.contains("def"))
+			{
+				Interpreter.def(stack, variables);
+			}
+			else if (element.contains("pop"))
+			{
+				Interpreter.pop(stack, variables);
+			}
+			return null;
+		}
+		else if (type == TYPE_INT)
+		{
+			return int_value;
+		}
+		else if (type == TYPE_DOUBLE)
+		{
+			return double_value;
+		}
+		else if (type == TYPE_BOOLEAN)
+			return null;
+		else if (type == TYPE_VARIABLE && element.contains("/"))
+			return element;
+		else if (type == TYPE_VARIABLE && !element.contains("/"))
+		{
+			Element e = variables.get(element);
+			return e.interpret(stack, null, variables);
+		}
+		else return null;
+	}
+
+	public String toString()
+	{
+		return element;
+	}
 }
