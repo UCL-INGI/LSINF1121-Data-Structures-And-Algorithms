31fdc2e839d76d23228997fba7b974f03c16e3d8
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-08-08 13:06:12 +0200

    Automatic synchronization from INGInious

--- m1interpreter/GeneratorPostScript.java
+++ m1interpreter/GeneratorPostScript.java
@@ -7,7 +7,9 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
 /**
- * Random generation of a valid derivation
+ * Random generation of a valid chain of PostScript instructions
+ * Note that the 'def' instruction is not generated here because of its complexity, 
+ * it's tested manually in another ScalaCheck property. 
  */
 public class GeneratorPostScript {
 	
@@ -31,101 +33,12 @@ public class GeneratorPostScript {
 		this.rand = new Random(seed); 
 	}
 	
-	/* Grade the student in the method main */
+	/* Main method not used for the grading */
 	public static void main(String[] args) {
 
-		GeneratorPostScript g = new GeneratorPostScript(77); 
+		/*GeneratorPostScript g = new GeneratorPostScript(77); 
 		String s = g.generate(30); 
-		System.out.println(s); 
-
-		/*Generator g = new Generator(42);
-		ScriptEngineManager manager = new ScriptEngineManager();
-    		ScriptEngine engine = manager.getEngineByName("JavaScript");  
-
-		int totPos = 100; 
-		int okPos = 0; 
-		int totNeg = 10; 
-		int okNeg = 0; 
-		int bug = 0; // unwanted exceptions with my own code, should not happen
-		int studentError = 0; // unwanted exceptions with the code of the student, means he failed
-
-		for (int i = 0 ; i < totPos ; i++) {
-			String[] res = g.generate(3); 
-			String expression = ""; 
-			for (int j = 0 ; j < res.length ; j++) {
-				expression += res[j]; 
-			}
-			//System.out.println("Expression : " + expression); 
-
-			// Evaluate the correct result with my own code
-			MyExpressionTree myTree;
-			String myResult = ""; 
-			try {
-				myTree = new MyExpressionTree(expression);
-				MyExpressionTree myDerivedTree = myTree.derive();
-				String myExpr = myDerivedTree.toString(); 
-				try {
-	    				Object result = engine.eval(myExpr.replace('x', '7').replace("sin", "Math.sin").replace("cos", "Math.cos").replace("--", "+"));
-					//System.out.println("ME : Evaluation of the derivative (x=7): " + result.toString()); 
-					myResult = result.toString(); 
-				} catch (ScriptException e) {
-					bug++; 
-					//System.out.println("ME : ScriptException"); 
-				}
-			} catch (MyParseException e) {
-				bug++; 
-				//System.out.println("ME : MyParseException... " + e + "\n");
-			}
-
-			// Evaluate the result of the student
-			ExpressionTree tree;
-			String studentResult = ""; 
-			try {
-				tree = new ExpressionTree(expression);
-				ExpressionTree derivedTree = tree.derive();
-				String expr = derivedTree.toString(); 
-				try {
-	    				Object result = engine.eval(expr.replace('x', '7').replace("sin", "Math.sin").replace("cos", "Math.cos").replace("--", "+"));
-					//System.out.println("STUDENT : Evaluation of the derivative (x=7): " + result.toString()); 
-					studentResult = result.toString(); 
-				} catch (ScriptException e) {
-					studentError++; 
-					//System.out.println("STUDENT : ScriptException"); 
-				}
-			} catch (Exception e) { // should be something like 'ParseException'
-				studentError++; 
-				//System.out.println("STUDENT : ParseException... " + e + "\n");
-			}
-			if (myResult.equals(studentResult))
-				okPos++; 
-		}
-
-		// Check if the student effectively throws an Exception upon wrong syntax
-		for (int i = 0 ; i < totNeg ; i++) {
-			String[] res = g.generateWrong(3); 
-			String expression = ""; 
-			for (int j = 0 ; j < res.length ; j++) {
-				expression += res[j]; 
-			}
-			//System.out.println("Expression : " + expression); 
-
-			ExpressionTree tree;
-			try {
-				tree = new ExpressionTree(expression);
-			} catch (Exception e) {
-				okNeg++; 
-				//System.out.println("ParseException correctly thrown");
-			}
-		}
-
-		// Compute the result of the student
-		if (bug != 0)
-			System.out.println("PROBLEM"); 
-		else if (okPos == totPos && okNeg == totNeg && studentError == 0)
-			System.out.println("OK " + okPos + " " + okNeg); 
-		else
-			System.out.println("KO " + totPos + " " + okPos + " " + totNeg + " " + okNeg + " " + studentError); */
-
+		System.out.println(s); */
 	}
 	
 	public  String generate(int maxDepth) {
@@ -144,7 +57,7 @@ public class GeneratorPostScript {
 
 		LinkedList<String> state = new LinkedList<String>(); 
 
-		for (int k = 0 ; k < maxDepth ; k++) { // TODO def
+		for (int k = 0 ; k < maxDepth ; k++) {
 			int i = rand.nextInt(11);
 			switch(i) {
 				case 0 : {
