012d75b098eb475e8a37932b1778e3e6cd7777fe
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-08-18 17:33:13 +0200

    Automatic synchronization from INGInious

--- m1stacktests/MyBuggyStack9.java
+++ m1stacktests/MyBuggyStack9.java
@@ -22,7 +22,7 @@ public class MyStack<E> implements Stack<E> {
 	}
 	
 	public E push(E item) { // add the element at the end instead of the top of the stack
-    	Stack<E> tempStack = new Stack<E>(); 
+    	java.util.Stack<E> tempStack = new java.util.Stack<E>(); 
     	for (int i = 0 ; i < size ; i++) {
         	tempStack.push(myStack.pop()); 
         }
