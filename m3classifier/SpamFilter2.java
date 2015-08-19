b6701fbda85733828e981270af72bf3a8dbdd687
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-08-19 11:18:13 +0200

    Automatic synchronization from INGInious

--- m3classifier/SpamFilter2.java
+++ m3classifier/SpamFilter2.java
@@ -6,18 +6,17 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
 
-public class SpamFilter2 {
+public class SpamFilter2 implements SpamFiltering {
 
-	private MyMap2<String, Word2> wordsMap;
+	private Map<String, WordInterface> wordsMap;
 	private HashSet<String> stopWords; 
 	
 	private ArrayList<String> hams;
 	private ArrayList<String> spams;
 
 	public SpamFilter2(String filename) {
-		wordsMap = new MyMap2<String, Word2>();
+		wordsMap = new MyMap2<String, WordInterface>();
 		stopWords = new HashSet<String>();
 		String[] stopList = {"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
 		for (String word : stopList) {
@@ -28,7 +27,7 @@ public class SpamFilter2 {
 		readInput(filename);
 	}
 	
-	public MyMap2<String, Word2> getWordsMap() {
+	public Map<String, WordInterface> getWordsMap() {
 		return wordsMap;
 	}
 
@@ -83,25 +82,25 @@ public class SpamFilter2 {
 			if (w == null)
 				w = new Word2(word); 
 			if (category.equals("ham")) {
-				w.incHamProba(1.0);
+				w.incHamProba(1);
 				Word2.totalNumberOfHams++; 
 			}
 			else {
-				w.incSpamProba(1.0);
+				w.incSpamProba(1);
 				Word2.totalNumberOfSpams++;
 			}
 			w.incOccurences();
 			current.put(word, w); // insert or replace
-            length++;
+			length++; 
 		}
 		
 		// Copy everything in the new map, after normalization
-		Iterator<Map.Entry<String, Word2>> it = current.entrySet().iterator(); 
+		Iterator<java.util.Map.Entry<String, Word2>> it = current.entrySet().iterator(); 
 		while (it.hasNext()) {
-			Map.Entry<String, Word2> pair = it.next(); 
+			java.util.Map.Entry<String, Word2> pair = it.next(); 
 			pair.getValue().normalize(length); 
 			if (wordsMap.containsKey(pair.getKey()))
-				wordsMap.get(pair.getKey()).add(pair.getValue());
+				((Word2) wordsMap.get(pair.getKey())).add(pair.getValue());
 			else
 				wordsMap.put(pair.getKey(), pair.getValue());
 		}
@@ -112,7 +111,7 @@ public class SpamFilter2 {
 		double pos = 0; 
 		double neg = 0; 
 		for (int i = 0 ; i < words.length ; i++) if (wordsMap.containsKey(words[i])){
-			double proba = wordsMap.get(words[i]).bayesProba(); 
+			double proba = ((Word2) wordsMap.get(words[i])).bayesProba(); 
 			pos += Math.log(proba);
 			neg += Math.log(1-proba); 
 		}
