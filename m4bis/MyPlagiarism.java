3c51aa6a906739281430d05eb68f882251ed7302
INGInious <no-reply@inginious.info.ucl.ac.be>
2015-11-11 12:51:12 +0100

    Automatic synchronization from INGInious

--- m4bis/MyPlagiarism.java
+++ m4bis/MyPlagiarism.java
@@ -42,7 +42,7 @@ public class MyPlagiarism implements PlagiarismInterface {
 
 			String line = reader.readLine();
 			while(line != null){
-				s += line;
+				s += line + "\n";
 				line = reader.readLine();
 			}          
 
