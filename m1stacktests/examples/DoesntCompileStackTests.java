f93d69f9945197e275f3d3e21087ddd659f675fc
Frédéric Kaczynski <frederic.kaczynski@student.uclouvain.be>
2016-08-03 16:11:46 +0200

    Commit

---
+++ m1stacktests/examples/DoesntCompileStackTests.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This class doesn't compile
+ * @author Frederic KACZYNSKI
+ */
+public class StackTests {
+    @Test
+    public void firstTest() {
+        asserue(false);
+    }
+}
