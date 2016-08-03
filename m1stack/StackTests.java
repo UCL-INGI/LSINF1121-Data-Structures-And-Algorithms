075df4bef5b1db40fd24668f965f5d08eec3be3b
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-03 16:15:17 +0200

    Automatic synchronization from INGInious

--- m1stack/StackTests.java
+++ m1stack/StackTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Frederic KACZYNSKI
@@ -34,5 +35,10 @@ public class StackTests {
         Stack<Integer> stack = new MyStack<>();
 
         assertEquals("Test of push", (Integer) 42, stack.push(42));
+        assertEquals("Test of push", (Integer) 2, stack.push(2));
+        assertEquals("Test of pop", (Integer) 2, stack.pop());
+        assertTrue("Test of empty", !stack.empty());
+        assertEquals("Test of pop", (Integer) 42, stack.pop());
+        assertTrue("Test of empty", stack.empty());
     }
 }
