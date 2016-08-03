c2da6aaebc6694a0bd448f950253890f21049919
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-03 14:27:17 +0200

    Automatic synchronization from INGInious

--- m1interpreter/InterpreterTests.java
+++ m1interpreter/InterpreterTests.java
@@ -7,8 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class InterpreterTests {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Result result = JUnitCore.runClasses(InterpreterTests.class);
 
         int returnCode = 0;
@@ -26,22 +25,46 @@ public class InterpreterTests {
         System.exit(returnCode);
     }
 
+    public void test(String message, String instr, String expected) {
+        Interpreter interpreter = new Interpreter();
+
+        assertEquals(message + " (" + instr + ")", expected, interpreter.interpret(instr));
+    }
+
     @Test
     public void testAdd() {
-        Interpreter interpreter = new Interpreter();
-
-        String instr = "4 3 add pstack";
-        String failMessage = "Test \"" + instr + "\"";
-        assertEquals(failMessage, "7", interpreter.interpret(instr));
+        test("Test of add", "4 3 add pstack", "7");
     }
 
     @Test
-    public void testSub()
-    {
-        Interpreter interpreter = new Interpreter();
+    public void testSub() {
+        test("Test of sub", "7 2 sub pstack", "5");
+    }
 
-        String instr = "7 2 sub pstack";
-        String failMessage = "Test \"" + instr + "\"";
-        assertEquals(failMessage, "5", interpreter.interpret(instr));
+    @Test
+    public void testMul() {
+        test("Test of mul", "42 5 mul pstack", "210");
+    }
+
+    @Test
+    public void testExch() {
+        test("Test of exch", "42 5 exch pstack", "5 42");
+    }
+
+    @Test
+    public void testDup() {
+        test("Test of dup", "42 5 dup pstack", "42 5 5");
+    }
+
+    @Test
+    public void testDef() {
+        test("Test of def", "/radius 2 def radius 4 mul pstack", "8");
+    }
+
+    @Test
+    public void testNoPstack() {
+        // Since there's no pstack, nothing should be displayed
+        test("Test without any pstack", "2 2 mul", "");
     }
 }
+
