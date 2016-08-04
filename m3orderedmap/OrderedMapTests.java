06d60ea3c268b2b1f3667c453b2b403b544a24f9
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-04 11:45:16 +0200

    Automatic synchronization from INGInious

---
+++ m3orderedmap/OrderedMapTests.java
@@ -0,0 +1,138 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Frederic KACZYNSKI
+ */
+public class OrderedMapTests {
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(OrderedMapTests.class);
+
+        int returnCode = 0;
+        if (!result.wasSuccessful())
+        {
+            for (Failure fail : result.getFailures())
+            {
+                System.out.println(fail.getMessage());
+                System.out.println();
+            }
+
+            returnCode = 1;
+        }
+
+        System.exit(returnCode);
+    }
+
+    @Test
+    public void firstTest() {
+
+        try {
+
+            OrderedMap tree = new SearchTree();
+
+            assertTrue(tree.isEmpty());
+            assertEquals(tree.size(), 0);
+
+            Set<String> value = new HashSet<>();
+            value.add("The Pretender");
+
+            assertEquals(tree.put("Foo Fighters", value), null);
+
+            assertEquals(tree.size(), 1);
+            assertEquals(tree.isEmpty(), false);
+
+            Set<String> result = tree.get("Foo Fighters");
+
+            assertEquals(result.size(), 1);
+            assertTrue(result.contains("The Pretender"));
+
+        } catch (Exception e) {
+            fail("Exception occured : " + e);
+        }
+
+    }
+
+    @Test
+    public void secondTest() {
+
+        try {
+
+            OrderedMap tree = new SearchTree();
+
+            Set<String> value = new HashSet<>();
+            value.add("Dynamite");
+            value.add("Poltron");
+
+            assertEquals(tree.put("ACDC", value), null);
+
+            value = new HashSet<>();
+            value.add("Autre musique");
+
+            assertEquals(tree.put("Autre groupe", value), null);
+
+            assertEquals(tree.size(), 2);
+            assertEquals(tree.isEmpty(), false);
+
+            Set<String> result = tree.get("Autre groupe");
+
+            assertEquals(result.size(), 1);
+            assertTrue(result.contains("Autre musique"));
+
+            result = tree.firstEntry().getValue();
+
+            assertEquals(result.size(), 2);
+            assertTrue(result.contains("Dynamite"));
+            assertTrue(result.contains("Poltron"));
+
+            result = tree.lastEntry().getValue();
+
+            assertEquals(result.size(), 1);
+            assertTrue(result.contains("Autre musique"));
+
+        } catch (Exception e) {
+            fail("Exception occured : " + e);
+        }
+
+
+    }
+
+    @Test
+    public void namedTest() {
+
+        try {
+
+            OrderedMap tree = new SearchTree("songs.txt");
+
+            assertEquals(tree.size(), 36);
+            assertEquals(tree.isEmpty(), false);
+
+            Set<String> result = tree.get("Van Halen");
+
+            assertEquals(result.size(), 1);
+            assertTrue(result.contains("You Really Got Me"));
+
+            List<Map.Entry<String, Set<String>>> resultBetween = tree.entriesBetween("Who", "ZZ Top");
+
+            assertEquals(resultBetween.size(), 3);
+
+
+
+        } catch (Exception e) {
+            fail("Exception occured : " + e);
+        }
+
+
+    }
+}
