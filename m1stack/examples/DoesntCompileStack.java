1070ef9d3b6f45ae2b0e6f9a73f20c97dce9f67a
Frédéric Kaczynski <frederic.kaczynski@student.uclouvain.be>
2016-08-05 09:28:11 +0200

    Commit

---
+++ m1stack/examples/DoesntCompileStack.java
@@ -0,0 +1,10 @@
+import java.util.EmptyStackException;
+
+/**
+ * This class doesn't compile because it doesn't implement
+ * the interface
+ * @author Frederic KACZYNSKI
+ */
+public class MyStack<E> implements Stack<E> {
+
+}
