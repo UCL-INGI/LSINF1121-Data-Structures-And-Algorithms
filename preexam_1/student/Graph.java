70ece413476bcc2e61d5d13937d4b2149a9e5e2d
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-09 15:33:16 +0200

    Automatic synchronization from INGInious

--- preexam_1/student/Graph.java
+++ preexam_1/student/Graph.java
@@ -1,40 +1,38 @@
+import java.util.ArrayList;
 
 public class Graph {
-
-    
-
     /**
      * @return the number of vertices
      */
     public int V() {
-        // ...
+        return 0;
     }
 
     /**
      * @return the number of edges
      */
     public int E() {
-        // ...
+        return 0;
     }
 
     /**
      * Add edge v-w to this graph
      */
     public void addEdge(int v, int w) {
-        // ...
+        return 0;
     }
 
     /**
      * @return the vertices adjacent to v
      */
     public Iterable<Integer> adj(int v) {
-        // ...
+        return new ArrayList<Integer>();
     }
 
     /**
      * @return a string representation
      */
     public String toString() {
-        // ...
+        return "Ne devrait pas être utilisé";
     }
 }
