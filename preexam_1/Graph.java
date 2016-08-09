4d4a504ac9dfb70b1ee69f85b5683699a22580a4
Frédéric Kaczynski <frederic.kaczynski@student.uclouvain.be>
2016-08-09 14:50:41 +0200

    Commit

---
+++ preexam_1/Graph.java
@@ -0,0 +1,38 @@
+
+public class Graph {
+
+    /**
+     * @return the number of vertices
+     */
+    public int V() {
+        // ...
+    }
+
+    /**
+     * @return the number of edges
+     */
+    public int E() {
+        // ...
+    }
+
+    /**
+     * Add edge v-w to this graph
+     */
+    public void addEdge(int v, int w) {
+        // ...
+    }
+
+    /**
+     * @return the vertices adjacent to v
+     */
+    public Iterable<Integer> adj(int v) {
+        // ...
+    }
+
+    /**
+     * @return a string representation
+     */
+    public String toString() {
+        // ...
+    }
+}
