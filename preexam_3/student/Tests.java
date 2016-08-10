1a22d17c14860d2a36f029c538dfd5576e86b071
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-10 14:39:18 +0200

    Automatic synchronization from INGInious

--- preexam_3/student/Tests.java
+++ preexam_3/student/Tests.java
@@ -6,6 +6,7 @@ import org.junit.runner.notification.Failure;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Tests {
 
