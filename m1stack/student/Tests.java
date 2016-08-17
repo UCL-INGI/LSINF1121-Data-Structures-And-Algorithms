f41ecc72290ee87e41e860eb0e5ea9c1b96a38e9
INGInious <no-reply@inginious.info.ucl.ac.be>
2016-08-17 15:54:18 +0200

    Automatic synchronization from INGInious

---
+++ m1stack/student/Tests.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class Tests {
+
+    @Test
+    public void testEmpty()
+    {
+        String message = "Test d'empty";
+        MyStack stack = new MyStack();
+        stack.push("test");
+        stack.pop();
+        assertTrue(message, stack.empty());
+    }
+
+    @Test
+    public void testNotEmpty()
+    {
+        String message = "Test d'empty";
+        MyStack stack = new MyStack();
+        stack.push("test");
+        assertFalse(message, stack.empty());
+    }
+}
