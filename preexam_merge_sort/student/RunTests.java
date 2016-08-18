12326103a386ac6eb4dcdaf8f0e838a8a3e10a77
Frédéric Kaczynski <frederic.kaczynski@student.uclouvain.be>
2016-08-18 10:52:08 +0200

    COmmit

--- preexam_merge_sort/student/RunTests.java
+++ preexam_merge_sort/student/RunTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.notification.Failure;
 import java.io.IOException;
 import java.lang.Process;
 import java.lang.InterruptedException;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 /**
  * This is a special class that runs the JUnit test and produce readable output.
@@ -14,7 +16,7 @@ import java.lang.InterruptedException;
  */
 public class RunTests {
 
-    public int maxErrorsForHints = 3;
+    public static int totalStacktrace = 10;
 
     public static void main(String[] args) throws IOException, InterruptedException {
         JUnitCore junit = new JUnitCore();
@@ -28,6 +30,7 @@ public class RunTests {
             System.out.println();
             System.out.println("::");
             System.out.println();
+            String output;
 
             for (Failure fail : result.getFailures()) {
                 // Only displays the exception thrown if it is not a "normal" exception thrown by JUnit
@@ -35,7 +38,19 @@ public class RunTests {
                 if (fail.getException() instanceof AssertionError) {
                     System.out.println("\t" + fail.getMessage());
                 } else {
-                    fail.getException().printStackTrace();
+                    Throwable e = fail.getException();
+
+                    StringWriter errors = new StringWriter();
+                    e.printStackTrace(new PrintWriter(errors));
+
+                    // We must put a \t behind each line for INGInious formatting
+                    // We also limit the number of lines
+                    String[] lines = errors.toString().split("\n");
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0;i < totalStacktrace;i++) {
+                        sb.append("\t" + lines[i] + "\n");
+                    }
+                    System.out.println(sb.toString());
                 }
             }
         }
