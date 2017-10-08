09ee2eb0d13d092eda748bdf9a3b1147c207956d
INGInious <no-reply@inginious.info.ucl.ac.be>
2017-10-08 17:33:21 +0200

    Automatic synchronization from INGInious

--- bilan_m1/NodeQueueTest.java
+++ bilan_m1/NodeQueueTest.java
@@ -156,7 +156,7 @@ public class NodeQueueTest {
     public void testGrade() {
         int score = 0;
 
-        try {
+        //try {
             if (testCorrect()) {
                 score += 50;
 
@@ -172,9 +172,9 @@ public class NodeQueueTest {
                 feedback("incorrect behavior :-100", score, false);
                 return;
             }
-        } catch (Exception e) {
-			feedback("incorrect behavior :-100", score, false);
-        }
+        //} catch (Exception e) {
+		//	feedback("incorrect behavior :-100", score, false);
+        //}
     }
 
     public static void main(String[] args) {
