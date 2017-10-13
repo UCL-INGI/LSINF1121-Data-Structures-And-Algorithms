1fa2afaa961bc60c2842dc43426da34fd3c863d0
INGInious <no-reply@inginious.info.ucl.ac.be>
2017-10-13 12:51:21 +0200

    Automatic synchronization from INGInious

--- bilan_m2_global_warming/GlobalWarmImplTest.java
+++ bilan_m2_global_warming/GlobalWarmImplTest.java
@@ -80,38 +80,11 @@ public class GlobalWarmingTest {
     }
 
 
-    public static boolean relaxedTestNbIsland(GlobalWarming gw, int expected) {
-        int res = gw.nbIslands();
-        return res == expected || res == (expected + (gw.altitude.length*gw.altitude.length)-gw.nbSafePointsCorrect(gw.waterLevel));
-    }
 
     public boolean testSafePointExam() {
-        System.out.println("safe points returned (should be 14):"+new GlobalWarmingImpl(getExamMatrix(),3).nbSafePoints(2));
-        return new GlobalWarmingImpl(getExamMatrix(),3).nbSafePoints(2) == 14;
+        System.out.println("safe points returned (should be 14):"+new GlobalWarmingImpl(getExamMatrix()).nbSafePoints(2));
+        return new GlobalWarmingImpl(getExamMatrix()).nbSafePoints(2) == 14;
     }
-
-    public boolean testIslandsExam() {
-        GlobalWarming g =  new GlobalWarmingImpl(getExamMatrix(),3);
-        boolean ok = relaxedTestNbIsland(g,4);
-        if (!ok) System.out.println("islands returned (should be 4):"+g.nbIslands());
-        return ok;
-    }
-
-    public boolean testOnSameIslandExam() {
-        GlobalWarming gw =  new GlobalWarmingImpl(getExamMatrix(),3);
-        boolean ok1 = gw.onSameIsland(new GlobalWarming.Point(1,3),new GlobalWarming.Point(3,4)) == false;
-        boolean ok2 = gw.onSameIsland(new GlobalWarming.Point(1,3),new GlobalWarming.Point(1,4)) == true;
-        boolean ok3 = gw.onSameIsland(new GlobalWarming.Point(1,3),new GlobalWarming.Point(2,3)) == true;
-        boolean ok4 = gw.onSameIsland(new GlobalWarming.Point(2,3),new GlobalWarming.Point(3,4)) == false;
-        System.out.println("onsameIslandsExam:"+ok1+" "+ok2+" "+ok3+" "+ok4);
-        return ok1 && ok2 && ok3 && ok4;
-    }
-
-    public boolean testShortestPathExam() {
-        List<GlobalWarming.Point> path = new GlobalWarmingImpl(getExamMatrix(), 3).shortestPath(new GlobalWarming.Point(1, 0), new GlobalWarming.Point(3, 1));
-        return path != null && path.size() == 4 && validPath(getExamMatrix(),3,point(1,0),point(3,1),path);
-    }
-
 
     public int [][] getRandomMatrix(int n,int bound) {
         int[][] matrix = new int[n][n];
@@ -130,7 +103,7 @@ public class GlobalWarmingTest {
 
     public boolean testSimpleAll() {
         int [][] matrix = getSimpleMatrix();
-        GlobalWarming warming = new GlobalWarmingImpl(matrix,0);
+        GlobalWarming warming = new GlobalWarmingImpl(matrix);
 
         if (warming.nbSafePoints(-1) != 100) {
             System.out.println("error in nbSafePoints");
@@ -144,58 +117,6 @@ public class GlobalWarmingTest {
 
         if (warming.nbSafePoints(1) != 0) {
             System.out.println("error in nbSafePoints");
-            return false;
-        }
-
-        if (!relaxedTestNbIsland(warming,4)) {
-
-            System.out.println("error in nbIslands"+ warming.nbIslands());
-            return false;
-        }
-
-        if (warming.onSameIsland(point(0,0),point(0,0))) {
-            System.out.println("error in onSameIsland");
-            return false;
-        }
-
-
-        if (warming.onSameIsland(point(0, 0), point(0, 1))) {
-            System.out.println("error in onSameIsland");
-            return false;
-        }
-
-        if (!warming.onSameIsland(point(4,5),point(1,7))) {
-            System.out.println("error in onSameIsland");
-            return false;
-        }
-
-        List<GlobalWarming.Point> path1 = warming.shortestPath(point(1,1),point(1,1));
-
-        if (!validPath(matrix,0,point(1,1),point(1,1),path1)) {
-            System.out.println("error in shortestPath");
-            return false;
-        }
-
-        if (!warming.shortestPath(point(9,9),point(9,9)).isEmpty()) {
-            System.out.println("error in shortestPath");
-            return false;
-        }
-
-        if (!warming.shortestPath(point(0,9),point(9,9)).isEmpty()) {
-            System.out.println("error in shortestPath");
-            return false;
-        }
-
-        List<GlobalWarming.Point> path2 = warming.shortestPath(point(4,5),point(1,7));
-        if (!validPath(matrix,0,point(4,5),point(1,7),path2)) {
-            System.out.println("error in shortestPath, path not valid");
-            return false;
-        }
-
-
-        if (path2.size() != 8) {
-            System.out.println("error in shortestPath, not correct length");
-            System.out.println(path2.size());
             return false;
         }
         return true;
@@ -216,7 +137,7 @@ public class GlobalWarmingTest {
     public boolean testCorrectnessNbSafePoints() {
         for (int i = 0; i < 100; i++) {
             int [][] matrix = getRandomMatrix(100,1000000);
-            GlobalWarming g1 = new GlobalWarmingImpl(matrix,0);
+            GlobalWarming g1 = new GlobalWarmingImpl(matrix);
 
             for (int l = 5; l < 10000000; l+= 10000) {
                 if (g1.nbSafePoints(l) != nSafePoints(matrix,l)) {
@@ -226,7 +147,7 @@ public class GlobalWarmingTest {
 
         }
         int [][] matrix = getSimpleMatrix();
-        GlobalWarming warming = new GlobalWarmingImpl(matrix,0);
+        GlobalWarming warming = new GlobalWarmingImpl(matrix);
 
         if (warming.nbSafePoints(-1) != 100) {
             System.out.println("error in nbSafePoints");
@@ -234,195 +155,9 @@ public class GlobalWarmingTest {
         }
 
 
-
         return true;
     }
 
-
-
-    public boolean testCorrectnessShortestPath() {
-        int level = 200000;
-        GlobalWarming.Point p1 = point(10,10);
-        GlobalWarming.Point p2 = point(15,15);
-
-        for (int k = 0; k < 50; k++) {
-            int [][] matrix = getRandomMatrix(50,1000000);
-            GlobalWarming g1 = new GlobalWarmingImpl(matrix,level);
-
-            for (int i = 0; i < 50; i++) {
-                for (int j = 0; j < 50-3; j++) {
-                    if (matrix[i][j] > level && matrix[i][j+1] > level && matrix[i][j+2] > level) {
-
-                        List<GlobalWarming.Point> path = g1.shortestPath(point(i,j),point(i,j+2));
-
-                        if (path.size() != 3 && !validPath(matrix,level,point(i,j),point(i,j+2),path)) {
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            List<GlobalWarming.Point> path1 = g1.shortestPath(point(10,10),point(15,15));
-            //List<GlobalWarming.Point> path2 = g1.shortestPath(point(10,10),point(15,15));
-            if (g1.onSameIsland(p1,p2) && !validPath(matrix,level,p1,p2,path1)) {
-                return false;
-            }
-        }
-        int [][] matrix = getSimpleMatrix();
-        GlobalWarming warming = new GlobalWarmingImpl(matrix,0);
-        List<GlobalWarming.Point> path1 = warming.shortestPath(point(1,1),point(1,1));
-
-        if (!validPath(matrix,0,point(1,1),point(1,1),path1)) {
-            return false;
-        }
-        if (!warming.shortestPath(point(9,9),point(9,9)).isEmpty()) {
-            return false;
-        }
-        if (!warming.shortestPath(point(0,9),point(9,9)).isEmpty()) {
-            return false;
-        }
-
-        List<GlobalWarming.Point> path2 = warming.shortestPath(point(4,5),point(1,7));
-        if (!validPath(matrix,0,point(4,5),point(1,7),path2)) {
-            return false;
-        }
-
-
-        if (path2.size() != 8) {
-            return false;
-        }
-
-
-        return true;
-    }
-
-
-    public boolean testCorrectnessOnSameIsland() {
-        int level = 200000;
-        GlobalWarming.Point p1 = point(10,10);
-        GlobalWarming.Point p2 = point(15,15);
-
-
-        for (int k = 0; k < 100; k++) {
-            int [][] matrix = getRandomMatrix(100,1000000);
-            GlobalWarming g1 = new GlobalWarmingImpl(matrix,level);
-
-            for (int i = 0; i < 100; i++) {
-                for (int j = 0; j < 100-3; j++) {
-                    if (matrix[i][j] > level && matrix[i][j+1] > level && matrix[i][j+2] > level) {
-                        if (!g1.onSameIsland(point(i,j),point(i,j+2))) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        int [][] matrix = getSimpleMatrix();
-        GlobalWarming warming = new GlobalWarmingImpl(matrix,0);
-
-        if (warming.onSameIsland(point(0,0),point(0,0))) {
-            return false;
-        }
-
-
-        if (warming.onSameIsland(point(0, 0), point(0, 1))) {
-            return false;
-        }
-
-        if (!warming.onSameIsland(point(4,5),point(1,7))) {
-            return false;
-        }
-
-
-        return true;
-    }
-
-    public boolean testCorrectnessNbIslands() {
-
-        int level = 200000;
-
-        {
-            int[][] matrix = getRandomMatrix(10, 10);
-
-            if (!relaxedTestNbIsland(new GlobalWarmingImpl(matrix, 20),0)) {
-                // all the bounds are under the sea level
-                return false;
-            }
-
-            if (!relaxedTestNbIsland(new GlobalWarmingImpl(matrix, -20),1)) {
-                // all the bounds are above the sea level
-
-                return false;
-            }
-
-            matrix[0][0] = 30;
-            matrix[0][1] = 30;
-            matrix[0][9] = 30;
-            matrix[9][0] = 30;
-            matrix[9][9] = 30;
-            matrix[9][8] = 30;
-
-            if (!relaxedTestNbIsland(new GlobalWarmingImpl(matrix, 25),4)) {
-                // all the bounds are above the sea level
-                return false;
-            }
-
-        }
-
-        for (int iter = 0; iter < 100; iter++) {
-
-
-            int[][] matrix = new int[100][100];
-
-            boolean [] generated = new boolean[10];
-            int nIslandExpected = 0;
-            for (int i = 0; i < rand.nextInt(10); i++) {
-                int k = rand.nextInt(8);
-                matrix[k*10][k*10] = 1;
-                matrix[k*10+1][k*10] = 1;
-                matrix[k*10][k*10+1] = 1;
-                matrix[k*10+1][k*10+1] = 1;
-                if (rand.nextBoolean()) {
-                    matrix[k*10+2][k*10+1] = 1;
-                }
-                if (rand.nextBoolean()) {
-                    matrix[k*10+2][k*10] = 1;
-                }
-                if (!generated[k]) {
-                    generated[k] = true;
-                    nIslandExpected += 1;
-                }
-            }
-            if (!relaxedTestNbIsland(new GlobalWarmingImpl(matrix, 0),nIslandExpected)) {
-                // all the bounds are above the sea level
-                return false;
-            }
-        }
-
-        int [][] matrix = getSimpleMatrix();
-        GlobalWarming warming = new GlobalWarmingImpl(matrix,0);
-        if (!relaxedTestNbIsland(warming,4)) {
-            return false;
-        }
-        return true;
-    }
-
-
-
-    public boolean validPath(int [][] matrix, int level, GlobalWarming.Point p1, GlobalWarming.Point p2, List<GlobalWarming.Point> path) {
-        for (GlobalWarming.Point p: path) {
-            if (matrix[p.x][p.y] <= level) return false;
-        }
-        for (int i = 0; i < path.size()-1; i++) {
-            if (!neighbors(path.get(i),path.get(i+1))) {
-                return false;
-            }
-        }
-        if (matrix[p1.x][p1.y] <= level && !path.isEmpty()) return false;
-        if (matrix[p2.x][p2.y] <= level && !path.isEmpty()) return false;
-
-        return !path.isEmpty() && path.get(0).equals(p1) && path.get(path.size()-1).equals(p2);
-    }
 
 
     public boolean neighbors(GlobalWarming.Point p1,GlobalWarming.Point p2) {
@@ -442,7 +177,7 @@ public class GlobalWarmingTest {
 
                     // do some computation here
                     long t0 = System.currentTimeMillis();
-                    GlobalWarming g = new GlobalWarmingImpl(matrix,1000000 );
+                    GlobalWarming g = new GlobalWarmingImpl(matrix);
                     long t1 = System.currentTimeMillis();
                     System.out.println("time constructor:"+(t1-t0));
 
@@ -462,7 +197,7 @@ public class GlobalWarmingTest {
         try {
 
             final int [][] matrix = getRandomMatrix(500,2000000);
-            final GlobalWarming g = new GlobalWarmingImpl(matrix,1000000 );
+            final GlobalWarming g = new GlobalWarmingImpl(matrix);
 
             boolean timeOk = new TimeLimitedCodeBlock() {
                 @Override
@@ -498,97 +233,8 @@ public class GlobalWarmingTest {
         return true;
     }
 
-    public boolean timeComplexityNbIslands() {
-        try {
+   
 
-            final int [][] matrix = getRandomMatrix(500,2000000);
-            final GlobalWarming g = new GlobalWarmingImpl(matrix,1000000 );
-
-            boolean timeOk = new TimeLimitedCodeBlock() {
-                @Override
-                public void codeBlock() {
-                    int max = 0;
-
-                    // do some computation here
-                    long t0 = System.currentTimeMillis();
-
-                    g.nbIslands();
-
-                    long t1 = System.currentTimeMillis();
-                    System.out.println("time nbIslands:"+(t1-t0));
-
-                }
-            }.run(5);
-            if (!timeOk) return false;
-            //Assert.assertTrue("correct time complexity", g.nCallsAdj < 3 * g.V());
-
-        } catch (AssertionError e) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean timeComplexityOnSameIsland() {
-        try {
-
-            final int [][] matrix = getRandomMatrix(500,2000000);
-            final GlobalWarming g = new GlobalWarmingImpl(matrix,1000000 );
-
-            boolean timeOk = new TimeLimitedCodeBlock() {
-                @Override
-                public void codeBlock() {
-                    int max = 0;
-                    // do some computation here
-                    long t0 = System.currentTimeMillis();
-                    int n = matrix.length;
-                    for (int i = 0; i < n; i++){
-                        for (int j = 0; j < n; j++) {
-                            g.onSameIsland(point(i,j),point(n-1,n-1));
-                        }
-                    }
-                    long t1 = System.currentTimeMillis();
-                    System.out.println("time onSameIsland:"+(t1-t0));
-                }
-            }.run(500);
-            if (!timeOk) return false;
-            //Assert.assertTrue("correct time complexity", g.nCallsAdj < 3 * g.V());
-
-        } catch (AssertionError e) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean timeComplexityShortestPath() {
-        try {
-
-            final int [][] matrix = getRandomMatrix(200,2000000);
-            final GlobalWarming g = new GlobalWarmingImpl(matrix,1000000 );
-
-            boolean timeOk = new TimeLimitedCodeBlock() {
-                @Override
-                public void codeBlock() {
-                    int max = 0;
-                    // do some computation here
-                    long t0 = System.currentTimeMillis();
-                    int n = matrix.length;
-                    for (int i = 0; i < n; i++){
-                        for (int j = 0; j < n; j++) {
-                            g.shortestPath(point(i,j),point(n-1,n-1));
-                        }
-                    }
-                    long t1 = System.currentTimeMillis();
-                    System.out.println("time shortestPath:"+(t1-t0));
-                }
-            }.run(500);
-            if (!timeOk) return false;
-            //Assert.assertTrue("correct time complexity", g.nCallsAdj < 3 * g.V());
-
-        } catch (AssertionError e) {
-            return false;
-        }
-        return true;
-    }
 
 
 
@@ -617,96 +263,39 @@ public class GlobalWarmingTest {
         int score = 0;
 
         try {
-
-            if (testOnSameIslandExam()) {
-                score += 15;
-            } else {
-                System.out.println("exam on same island -15");
-            }
-            if (testShortestPathExam()) {
-                score += 15;
-            } else {
-                System.out.println("exam shortest path -15");
-            }
+           
             if (testSafePointExam()) {
-                score += 15;
+                score += 10;
             } else {
-                System.out.println("exam safe point -15");
+                System.out.println("exam safe point -10");
             }
-            if (testIslandsExam()) {
-                score += 15;
-            } else {
-                System.out.println("exam islands -15");
-            }
-
-            /*
+ 
             if (testSimpleAll()) {
-                score += 5;
+                score += 10;
 
                 if (timeComplexityConstructorCorrect()) {
-                    score += 5;
+                    score += 20;
                 } else {
-                    feedback("test complexity constructor:-10");
+                    feedback("test complexity constructor:-20");
                 }
 
             } else {
                 feedback("test correctness all methods and constructor :-10");
-            }*/
+            }
 
 
             if (testCorrectnessNbSafePoints()) {
-                score += 5;
+                score += 30;
 
                 if (timeComplexityNbSafePoints()) {
-                    score += 5;
+                    score += 30;
                 } else {
-                    feedback("test complexity nbSafePoints:-10");
+                    feedback("test complexity nbSafePoints:-30");
                 }
 
             } else {
-                feedback("test correctness NbSafePoints on random graphs:-20");
+                feedback("test correctness NbSafePoints on random graphs:-30");
             }
-
-
-            if (testCorrectnessShortestPath()) {
-                score += 10;
-
-                if (timeComplexityShortestPath()) {
-                    score += 10;
-                } else {
-                    feedback("test complexity shortestPath (hint:could you stop earlier, do you really need to visit all positions?):-10");
-                }
-
-            } else {
-                feedback("test correctness Path on random graphs:-20");
-            }
-
-
-            if (testCorrectnessOnSameIsland()) {
-                score += 10;
-
-                if (timeComplexityOnSameIsland()) {
-                    score += 10;
-                } else {
-                    feedback("test complexity nbIslands:-10");
-                }
-            } else {
-                feedback("test correctness onSameIsland on random graphs:-20");
-            }
-
-
-            if (testCorrectnessNbIslands()) {
-                score += 10;
-
-                if (timeComplexityNbIslands()) {
-                    score += 10;
-                } else {
-                    feedback("test complexity nbIslands:-10");
-                }
-            } else {
-                feedback("test correctness nbIslands on random graphs:-20");
-            }
-
 
         } catch (Exception e) {
 
